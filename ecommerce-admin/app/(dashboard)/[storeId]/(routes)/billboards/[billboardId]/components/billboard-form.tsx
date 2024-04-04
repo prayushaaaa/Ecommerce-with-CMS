09ba3c42aa8451e6f74beb4dd5bd17e805732734
Prayusha Acharya <prayusha000@gmail.com>
2024-04-04 07:12:21 +0545

    ignore

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -54,10 +54,14 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onSubmit = async (data: BillboardFormValues) => {
         try {
             setLoading(true);
-            await axios.patch(`/api/stores/${params.storeId}`, data);
+            if (initialData) {
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+            }
+            else {
+                await axios.post(`/api/${params.storeId}/billboards`, data);
+            }
             router.refresh();
-
-            toast.success("Store updated");
+            toast.success(toastMessage);
         } catch (error) {
             toast.error("Something went wrong!");
         }
@@ -69,15 +73,14 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/stores/${params.storeId}`);
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+
             router.push("/");
             router.refresh();
-
-
             toast.success("Store Deleted");
 
         } catch (error) {
-            toast.error("Make sure theer are no active products and categories.")
+            toast.error("Make sure there are no active products and categories in this billboard.")
         }
         finally {
             setLoading(false);
@@ -161,3 +164,5 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 };
 
 export default BillboardForm;
+
+//git test
