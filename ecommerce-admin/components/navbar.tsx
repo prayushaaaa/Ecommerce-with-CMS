555b2489fba0aca7aeb6c0feef021f5e8d4c0c5a
Prayusha Acharya <prayusha000@gmail.com>
2024-04-12 00:01:28 +0545

    add dark theme

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -4,6 +4,7 @@ import { MainNav } from '@/components/main-nav';
 import StoreSwitcher from '@/components/store-switcher';
 import { redirect } from 'next/navigation';
 import prismadb from '@/lib/prismadb';
+import { ModeToggle } from '@/components/theme-toggle';
 
 const NavBar = async () => {
     const { userId } = auth();
@@ -25,6 +26,7 @@ const NavBar = async () => {
                 <StoreSwitcher items={stores} />
                 <MainNav className='mx-6' />
                 <div className='ml-auto flex items-center space-x-4'>
+                    <ModeToggle />
                     <UserButton afterSignOutUrl='/' />
                 </div>
             </div>
